387eacb4da8ba992e1092bb96390d0bd50b468ef
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-11-09 23:00:26 +0200

    hist eq works imvluding rgb

--- ex01/histogramEqualize.m
+++ ex01/histogramEqualize.m
@@ -1,9 +1,24 @@
 function [imEq, histOrig, histEq] = histogramEqualize(imOrig)   
-    imInt = uint8(imOrig * 256);
+    
+    dims = ndims(imOrig);
+    
+    if (dims == 3)
+        imYIQ = transformRGB2YIQ(imOrig);
+        imInt = uint8(imYIQ(:,:,1) * 256);
+    else
+        imInt = uint8(imOrig * 256);
+    end  
+    
     [histOrig, bins] = imhist(imInt);
     cumHist = cumsum(histOrig);    
     N = prod(size(imOrig));
     cumHistInt = uint8( (cumHist * 255) / N);
     imEq = intlut(imInt, cumHistInt);       
     [histEq, bins] = imhist(imEq);
+    
+    if (dims == 3)        
+        imYIQ(:,:,1) = double(imEq) / 256;
+        imEq = transformYIQ2RGB(imYIQ);        
+    end
+    
 end
