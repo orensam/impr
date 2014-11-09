a5b1c686ef48f5e24bfdf00418a915629e2f5816
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-11-09 19:07:12 +0200

    basic hist eq works

--- ex01/histogramEqualize.m
+++ ex01/histogramEqualize.m
@@ -1,3 +1,9 @@
-function [imEq, histOrig, histEq] = histogramEqualize(imOrig)
-    
+function [imEq, histOrig, histEq] = histogramEqualize(imOrig)   
+    imInt = uint8(imOrig * 256);
+    [histOrig, bins] = imhist(imInt);
+    cumHist = cumsum(histOrig);    
+    N = prod(size(imOrig));
+    cumHistInt = uint8( (cumHist * 255) / N);
+    imEq = intlut(imInt, cumHistInt);       
+    [histEq, bins] = imhist(imEq);
 end
