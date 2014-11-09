83b52e93f3633bfe1c4863cb07f22924670030d9
Oren Samuel <orensam@gmail.com>
2014-11-10 00:03:02 +0200

    fix rgb-yiq conversions

--- ex01/transformRGB2YIQ.m
+++ ex01/transformRGB2YIQ.m
@@ -1,8 +1,8 @@
 function [imYIQ] = transformRGB2YIQ(imRGB)    
     T = [0.299, 0.587, 0.114; 
          0.596, -0.275, -0.321
-         0.212, -0.523, 0.311]';             
+         0.212, -0.523, 0.311];             
     imRGB2 = reshape(imRGB, [], 3);
-    imYIQ = imRGB2 * T;
-    imYIQ = reshape(imYIQ, size(imRGB));
+    imYIQ = T * imRGB2';
+    imYIQ = reshape(imYIQ', size(imRGB));
 end
