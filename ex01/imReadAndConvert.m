eb32f5d7333d5e75b2e44391fe4b8c7bf20678fa
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-11-12 20:35:35 +0200

    imread doc

--- ex01/imReadAndConvert.m
+++ ex01/imReadAndConvert.m
@@ -1,4 +1,7 @@
 function newImage = imReadAndConvert(filename, representation)
+    % This function receives an image filename and a required
+    % representation (1 GS, 2 RGB), and returns the image in doubles,
+    % after conversion of RGB to grayscale if desired.
     image = imread(filename);
     newImage = double(image) / 255;
     imInfo = imfinfo(filename);
