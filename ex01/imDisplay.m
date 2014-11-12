f3ef3e79dbb0070369e5d8c7f93a079274fec45f
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-11-12 20:34:19 +0200

    imDisplay doc

--- ex01/imDisplay.m
+++ ex01/imDisplay.m
@@ -1,4 +1,6 @@
 function [] = imDisplay(filename, representation)
+    % This function receives an image filename and requested represntation,
+    % and shows the given image in that representation.
     image = imReadAndConvert(filename, representation);    
     imshow(image);
     impixelinfo;
