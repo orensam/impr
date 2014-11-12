38892f1d6619bd04b2db695772ceb71b790160a2
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-11-12 19:05:07 +0200

    hist eq - added stretching and doc

--- ex01/histogramEqualize.m
+++ ex01/histogramEqualize.m
@@ -1,7 +1,12 @@
 function [imEq, histOrig, histEq] = histogramEqualize(imOrig)   
+    % This function, given an image (in doubles),
+    % Equalizes its histogram (or its Y channel histogram in case
+    % of color image), displays the old and new images,
+    % and returns the equalized image, and the old and new
+    % histograms.
     
-    dims = ndims(imOrig);
-    
+    % Get image as ints, and handle color images
+    dims = ndims(imOrig);    
     if (dims == 3)
         imYIQ = transformRGB2YIQ(imOrig);
         imInt = uint8(imYIQ(:,:,1) * 255);
@@ -9,17 +14,46 @@ function [imEq, histOrig, histEq] = histogramEqualize(imOrig)
         imInt = uint8(imOrig * 255);
     end  
     
+    % Equalize the histogram according to lecture spec
     [histOrig, bins] = imhist(imInt);
     cumHist = cumsum(histOrig);
-    N = prod(size(imInt));
+    N = numel(imInt);
     cumHistInt = uint8( (cumHist * 255) / N);
+    % This is where the image values are replaced
     imEq = intlut(imInt, cumHistInt);       
-    [histEq, bins] = imhist(imEq);
-    imEq = double(imEq) / 255;
     
+    % Stretch histogram linearly over [0,255] (if it isn't already so)
+    mini = min(imEq(:));        
+    if (mini ~= 0)
+        imEq = imEq - mini;
+    end    
+    maxi = max(imEq(:));    
+    if (maxi ~= 255)
+        imEq = imEq .* (255 / maxi); 
+    end
+    
+    % Get the new histogram
+    [histEq, bins] = imhist(imEq);
+    
+    
+    % Handle return values and color image rebuild
+    imEq = double(imEq) / 255;
     if (dims == 3)        
         imYIQ(:,:,1) = imEq;
         imEq = transformYIQ2RGB(imYIQ);        
     end
     
+    % Display the old and new image
+    dispImages(imOrig, imEq);
+end
+
+function [] = dispImages(imOrig, imEq)
+    % Displays the original and equalized images side by side.
+    figure;
+    subplot(1,2,1);
+    imshow(imOrig);
+    title('Original Image');
+    subplot(1,2,2);
+    imshow(imEq);
+    title('Equalized Image');
 end
