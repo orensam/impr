35c24e931cddd8470705e91e7e6a1cc3d5cf7f42
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-18 18:03:40 +0200

    doc

--- ex03/expand.m
+++ ex03/expand.m
@@ -1,4 +1,7 @@
 function [expandedImage] = expand(im, filter)
+    % Expands the size of the given image by 2 in each diemnsion.
+    % Uses the given filter to blur the image afterwords.
+    
     filter = 2 * filter;    
     expandedImage = zeros(2 * size(im));
     expandedImage(1:2:end, 1:2:end) = im;
