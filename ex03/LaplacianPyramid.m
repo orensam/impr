648ce4644481e2f5e518b5d29b57d2844e0cdc7e
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-18 18:03:25 +0200

    doc, index fix

--- ex03/LaplacianPyramid.m
+++ ex03/LaplacianPyramid.m
@@ -1,11 +1,11 @@
 function [pyr, filter] = LaplacianPyramid(im, maxLevels, filterSize)
-% Creates the Laplacian pyramid of the given image, returned in the pyr cell 
-% array. It is calculated using the Gaussian pyramid.
-% Pyramid height is at most maxLevels. Can be smaller if a reduction
-% Makes the image smaller than 16*16.
-% filterSize specifies the size of the blur filter for the Gaussain
-% pyramid, and it is doubled for the exapnd-blur done here.
-% This filter (not-doubled) is also returned.
+    % Creates the Laplacian pyramid of the given image, returned in the pyr cell 
+    % array. It is calculated using the Gaussian pyramid.
+    % Pyramid height is at most maxLevels. Can be smaller if a reduction
+    % Makes the image smaller than 16*16.
+    % filterSize specifies the size of the blur filter for the Gaussain
+    % pyramid, and it is doubled for the exapnd-blur done here.
+    % This filter (not-doubled) is also returned.
 
     [gpyr, filter] = GaussianPyramid(im, maxLevels, filterSize);
     nLevels = numel(gpyr);
@@ -14,7 +14,7 @@ function [pyr, filter] = LaplacianPyramid(im, maxLevels, filterSize)
     for i=1:nLevels - 1
         pyr{i} = gpyr{i} - expand(gpyr{i+1}, filter);    
     end
-    pyr{nLevels} = gpyr{i+1};
+    pyr{nLevels} = gpyr{nLevels};
     
 end
 
