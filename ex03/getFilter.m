46ca7b4ba2211c3113cc49acdb88905f64264247
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-18 18:03:59 +0200

    doc

--- ex03/getFilter.m
+++ ex03/getFilter.m
@@ -1,4 +1,7 @@
 function [filter] = getFilter(filterSize)
+    % Returns a gaussian filter (row vector) of the given size.
+    % The filter is normalized to have sum 1.
+    
     k = 1:filterSize-1;
     filter = [1 cumprod((filterSize-k)./k)];
     filter = filter / sum(filter);
