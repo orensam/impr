b19174516c9410b52d4e6cc43fab6d60717c962d
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-18 17:19:07 +0200

    disp pyr and render pyr

---
+++ ex03/displayPyramid.m
@@ -0,0 +1,6 @@
+function [] = displayPyramid(pyr,levels)
+    % Renders the given pyramid and displays the result.
+    
+    res = renderPyramid(pyr, levels);
+    imshow(res);
+end
---
+++ ex03/renderPyramid.m
@@ -0,0 +1,13 @@
+function res = renderPyramid(pyr,levels)
+    % Renders the given pyramid to a single image.
+    % Uses a white background.
+    
+    [nrows, ncols] = cellfun(@size, pyr)
+    res = ones(nrows(1), sum(ncols));
+    colBorders = [0 cumsum(ncols)]
+    
+    for i = 1:levels
+        res(1:nrows(i), colBorders(i)+1:colBorders(i+1)) = pyr{i};
+    end
+    
+end
