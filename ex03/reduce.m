cccfee6d3f4eec02311b637a307a9903f8335e21
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-18 18:04:25 +0200

    doc

--- ex03/reduce.m
+++ ex03/reduce.m
@@ -1,4 +1,7 @@
-function [reducedImage] = reduce(im, filter)    
+function [reducedImage] = reduce(im, filter)
+    % Reduces the size of the given image by 2 in each diemnsion.
+    % Uses the given filter to blur the image beforehand.
+    
     blurredImage = conv2(conv2(im, filter, 'same'), filter', 'same');
     reducedImage = blurredImage(1:2:end, 1:2:end);
 end
