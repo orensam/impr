b9a9bb54e70875be6b65dd9aecd29d458f7576df
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-18 18:03:03 +0200

    doc

--- ex03/GaussianPyramid.m
+++ ex03/GaussianPyramid.m
@@ -1,10 +1,10 @@
 function [pyr, filter] = GaussianPyramid(im, maxLevels, filterSize)
-% Creates the Gassian pyramid of the given image, returned in the pyr cell 
-% array. 
-% Pyramid height is at most maxLevels. Can be smaller if a reduction
-% Makes the image smaller than 16*16.
-% filterSize specifies the size of the Gaussian filter used in the reduce
-% process. This filter is also returned.
+    % Creates the Gassian pyramid of the given image, returned in the pyr cell 
+    % array. 
+    % Pyramid height is at most maxLevels. Can be smaller if a reduction
+    % Makes the image smaller than 16*16.
+    % filterSize specifies the size of the Gaussian filter used in the reduce
+    % process. This filter is also returned.
     
     % Calculate number of levels so we don't reach an image size
     % less than 16x16
