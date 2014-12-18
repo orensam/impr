9d33fe5b065594bf82a2a0da40fabd23f2e5d995
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-18 17:59:27 +0200

    imread copy from ex1

---
+++ ex03/imReadAndConvert.m
@@ -0,0 +1,24 @@
+function newImage = imReadAndConvert(filename, representation)
+    % This function receives an image filename and a required
+    % representation (1 GS, 2 RGB), and returns the image in doubles,
+    % after conversion of RGB to grayscale if desired.
+    try
+        image = imread(filename);
+    catch
+        newImage = 0;
+        fprintf('Error: no such file\n');        
+        return;
+    end
+    
+    newImage = double(image) / 255;
+    
+    try
+        imInfo = imfinfo(filename);
+    catch
+        fprintf('Error: file is not a valid image\n');
+    end
+    
+    if strcmp(imInfo.ColorType, 'truecolor') && representation == 1
+        newImage = rgb2gray(newImage);     
+    end
+end
