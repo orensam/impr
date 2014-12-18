d318498750418a115c9d2a2731e0a4ff75de9fa1
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-18 17:59:46 +0200

    pyr blending works

---
+++ ex03/pyramidBlending.m
@@ -0,0 +1,18 @@
+function [imBlend] = pyramidBlending(im1, im2, mask, maxLevels, filterSizeIm, filterSizeMask)
+    % Blends the two given images using the pyramid method, with the given
+    % mask deciding on the which areas to use from which image.
+    
+    [lpyr1, filter] = LaplacianPyramid(im1, maxLevels, filterSizeIm);
+    [lpyr2, ~] = LaplacianPyramid(im2, maxLevels, filterSizeIm);
+    
+    [mpyr, ~] = GaussianPyramid(mask, maxLevels, filterSizeMask);
+    
+    nLevels = numel(mpyr);
+    respyr = cell(1, nLevels);
+    for k = 1:nLevels
+        respyr{k} = mpyr{k} .* lpyr1{k} + (1-mpyr{k}) .* lpyr2{k};
+    end
+    
+    imBlend = LaplacianToImage(respyr, filter, ones(1, nLevels));
+    
+end
