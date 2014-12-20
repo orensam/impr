7cf25a749a89bd2fc56f5cc07635d13c7d438ddd
Oren Samuel <orensam@gmail.com>
2014-12-20 14:24:07 +0200

    first example

--- ex03/blendingExample1.m
+++ ex03/blendingExample1.m
@@ -1,22 +1,5 @@
-function [] = blendingExample1()
-    % First RGB blending example
-    
-    im1 = imReadAndConvert('images/cat.jpg',2);
-    im2 = imReadAndConvert('images/batman.jpg',2);
-    mask = imReadAndConvert('images/mask1.png',1);
-    res = pyramidBlendingRGB(im1, im2, mask, 3, 15, 21);
-    
-    figure;
-    imshow(im1);
-    title('Image 1');
-    figure;
-    imshow(im2);
-    title('Image 2');
-    figure;
-    imshow(mask);
-    title('Blending mask');
-    figure;
-    imshow(res);
-    title('Blended image');
-    
-end
+% First RGB blending example
+im1 = imReadAndConvert('cat.jpg', 2);
+im2 = imReadAndConvert('elephant.jpg', 2);
+mask = round(imReadAndConvert('mask1.jpg', 1));
+pyramidBlendingRGB(im1, im2, mask, 7, 75, 55);
