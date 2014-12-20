a7a918932f6151133e801a4e170b2f94df070230
Oren Samuel <orensam@gmail.com>
2014-12-20 17:34:40 +0200

    exmaples

--- ex03/blendingExample1.m
+++ ex03/blendingExample1.m
@@ -1,4 +1,4 @@
-% First RGB blending example
+% First RGB blending example: Catephant!
 im1 = imReadAndConvert('cat.jpg', 2);
 im2 = imReadAndConvert('elephant.jpg', 2);
 mask = round(imReadAndConvert('mask1.jpg', 1));
