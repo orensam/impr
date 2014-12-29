42804866f140d6715e6c58f0e4de4b557a4ac816
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-12-29 16:58:11 +0200

    load imgs

---
+++ ex04/loadImages.m
@@ -0,0 +1,26 @@
+function imgs = loadImages(directoryPath)
+%
+% Read all images from directoryPath
+%
+% Arguments:
+% directoryPath − A string with the directory path
+%
+% Returns
+% imgs − 4 dimensional vector, where imgs(:,:,:,k) is the k−th
+% image in RGB format.
+
+    filenames = dir(directoryPath);
+    filenames(1:2) = [];
+    
+    imSize = size(imread(getPath(directoryPath, filenames(1).name)));
+    imgs = zeros(imSize(1), imSize(2), imSize(3), numel(filenames));
+    
+    for i = 1:numel(filenames)
+        imgs(:,:,:,i) = double(imread(getPath(directoryPath, filenames(i).name))) / 255;
+    end
+    
+end
+
+function fn = getPath(directory, name)
+    fn = strcat(directory, '/', name);
+end
