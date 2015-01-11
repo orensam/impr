f76dc557ec11b12bd65791cc81851cd1d5aaf8fc
Oren Samuel <orensam@gmail.com>
2015-01-12 00:01:37 +0200

    test

--- ex04/test.m
+++ ex04/test.m
@@ -1,10 +1,10 @@
 minMatchScore = 0.5;
 ransacIters = 1000;
-ransacInlierTol = 500;
+ransacInlierTol = 50;
 
-imgs = loadImages('apples');
-im1 = rgb2gray(imgs(:,:,:,75));
-im2 = rgb2gray(imgs(:,:,:,76));
+imgs = loadImages('apples2');
+im1 = rgb2gray(imgs(:,:,:,3));
+im2 = rgb2gray(imgs(:,:,:,4));
 pyr1 = GaussianPyramid(im1, 3, 3);
 pyr2 = GaussianPyramid(im2, 3, 3);
 [pos1, desc1] = findFeatures(pyr1, 100);
@@ -12,7 +12,7 @@ pyr2 = GaussianPyramid(im2, 3, 3);
 [ind1, ind2] = myMatchFeatures(desc1, desc2, minMatchScore);
 newPos1 = pos1(ind1,:);
 newPos2 = pos2(ind2,:);
-[T, inliers] = ransacTransform(newPos1, newPos2, ransacIters, ransacInlierTol);
+[T, inliers] = ransacTransform(newPos2, newPos1, ransacIters, ransacInlierTol);
 T
 displayTheMatches(im1, im2, newPos1, newPos2, inliers);
 
