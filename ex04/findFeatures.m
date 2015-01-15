a6781169eb54a9d1d448498a3a47f6530fc12a55
Oren Samuel <oren.samuel@mail.huji.ac.il>
2015-01-15 12:14:46 +0200

    fix huge bug and now things actually work

--- ex04/findFeatures.m
+++ ex04/findFeatures.m
@@ -8,24 +8,30 @@ function [pos, desc] = findFeatures(pyr, maxNum)
 % coordinates are provided at the pyramid level pyr{1}.
 % desc - A kxkxn feature descriptor matrix.
 
-    N = 2;
-    M = 2;
+    N = 8;
+    M = 8;
     DESC_RAD = 3;
             
     % Get corners in pyramid level 1
     pos = spreadOutCorners(pyr{1}, N, M, maxNum);
         
     % Convert to level 3 positions
-    level3Pos = 0.25 * (pos - 1) + 1; % 2^(l_1 - l_2) = 2^(1 - 3) = 1/4
+    level3Pos = ((pos-1)/4) + 1; % 2^(l_1 - l_2) = 2^(1 - 3) = 1/4
     
     % Fix coordinates - so that all positions can be interpolated.
     [level3Height, level3Width] = size(pyr{3});
     cols = level3Pos(:,1);
     rows = level3Pos(:,2);
-    colMask = (cols > (DESC_RAD + 1)) & (cols < (level3Width - DESC_RAD));
-    rowMask = (rows > (DESC_RAD + 1)) & (rows < (level3Height - DESC_RAD));
-    mask = colMask & rowMask;
+    colMask = (cols > (DESC_RAD)) & (cols < (level3Width - DESC_RAD));
+    rowMask = (rows > (DESC_RAD)) & (rows < (level3Height - DESC_RAD));
+    mask = colMask & rowMask;    
     level3Pos = [cols(mask), rows(mask)];    
+    
+    % Remove problematic coordinates from the original positions
+    % as well
+    level1Cols = pos(:,1);
+    level1Rows = pos(:,2);
+    pos = [level1Cols(mask), level1Rows(mask)];    
     
     % Get the descriptors for the feature points we found
     desc = sampleDescriptor(pyr{3}, level3Pos, DESC_RAD);
