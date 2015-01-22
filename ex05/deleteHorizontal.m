760914902e5a7e0d7054b94005803e45b000132d
Oren Samuel <oren.samuel@mail.huji.ac.il>
2015-01-22 19:22:00 +0200

    delete horizontal works

--- ex05/deleteHorizontal.m
+++ ex05/deleteHorizontal.m
@@ -1,3 +1,21 @@
 function [outImage] = deleteHorizontal(image, lowFilt, highFilt, levels)
-%
+% Deletes horizontal lines from a page image.
+    
+    [h, w] = size(image);
+    wd = DWT(image, lowFilt, highFilt, levels);
+    
+    top=1;
+    bottom=h/2
+    left=w/2
+    right=w
+    
+    for i = 1:levels
+        wd(top:bottom, left+1:right)=0;
+        bottom=bottom/2
+        left=left/2
+        right=right/2
+    end
+    
+    outImage = IDWT(wd, lowFilt, highFilt, levels);
+        
 end
