e335547bc9084a560de89d66b361a2b669bc7880
Oren Samuel <orensam@gmail.com>
2015-01-28 19:20:14 +0200

    fix call to denoise

--- ex05/filterNoisyImage.m
+++ ex05/filterNoisyImage.m
@@ -1,5 +1,5 @@
-image = im2double(imread('myNoisyImage.png'));
+im = im2double(imread('myNoisyImage.png'));
 lowFilt = [1 1]./2;
 highFilt = [1 -1]./2;
 levels = 2;
-dn = denoising(im, lowFilt, [0.5 0.5], highFilt, levels);
+dn = denoising(im, lowFilt, highFilt, levels);
