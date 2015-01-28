a0da8abedcc51c10cc3fe4fd22eb17dddd7a8135
Oren Samuel <orensam@gmail.com>
2015-01-28 19:02:10 +0200

    doc

--- ex05/denoising.m
+++ ex05/denoising.m
@@ -1,20 +1,24 @@
 function [denoiseImage] = denoising(image, lowFilt, highFilt, levels)
 % Denoises the given image using DWT and displays the result.
-
-    threshold = 0.06;
     
     [h, w] = size(image);
+    % Threshold found by trial and error
+    threshold = 0.06;            
     
+    % Get wavelet representation
     wd = DWT(image, lowFilt, highFilt, levels);    
     
+    % Find position of non-LL coefficients
     LLHeight = h / 2^levels;
     LLWidth = w / 2^levels;    
     mask = ones(size(image));
     mask(1:LLHeight, 1:LLWidth) = 0;
     
-    wd(abs(wd) < threshold & mask) = 0;
+    % perform thresholding
+    wd((abs(wd) < threshold) & mask) = 0;
     denoiseImage = IDWT(wd, lowFilt, highFilt, levels);
-
+    
+    % Display the result
     figure;
     subplot(1,2,1);
     imshow(image);
